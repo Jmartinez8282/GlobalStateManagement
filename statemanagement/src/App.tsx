13cd54063b61fb76046d25a19d56401f3dc16827
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2024-10-22 17:19:06 -0700

    first commit

---
+++ statemanagement/src/App.tsx
@@ -0,0 +1,15 @@
+import Counter from "./components/counter"
+
+
+const App = () => {
+  return (
+  <>
+  <h1 className="text-center">Global State Management</h1>
+
+  
+  <Counter/>
+  </>
+  )
+}
+
+export default App
